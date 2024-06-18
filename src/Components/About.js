46309390680683f4f8513ac0a24fce8e2822a614
Ayomide Aderotoye <ayomide.aderotoye@gmail.com>
2024-06-18 23:12:00 +0100

    Initial Commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,6 +2,7 @@ import React from 'react'
 import AboutBackground from '../Assets/about-background.png';
 import AboutBackgroundImage from '../Assets/about-background-image.png';
 import { BsFillPlayCircleFill } from 'react-icons/bs';
+
 const About = () => {
   return (
     <div className='about-section-container'>
@@ -17,7 +18,7 @@ const About = () => {
                 Food Is An Important Part Of A Balanced Diet
             </h1>
             <p className='primary-text'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugit labore sequi! Dicta harum placeat quam obcaecati deleniti vitae esse, voluptatem facere!
+                Lorem ipsum dolor hryu amet consectetur jgwfhisicing elit. Aspernatur fugit labore sequi! Dicta harum placeat quam obcaecati deleniti vitae esse, voluptatem facere!
             </p>
             <p className='primary-text'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos minima fuga enim ea? Accusantium, quae ut.
